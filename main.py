4181e763ceb52d512886d771dbc39227470ea0ca
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-06-21 13:00:46 +0300

    svm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
 from sklearn import preprocessing
 
 #genders
@@ -7,8 +8,8 @@ genders = ['male', 'female']
 
 #[height, weight, shoe_size]
 X = [[188, 80, 44],
-     [188, 80, 44],
-     [188, 80, 44],
+     [188, 87, 45],
+     [183, 79, 42],
      [177, 70, 43],
      [160, 60, 38],
      [154, 54, 37],
@@ -17,29 +18,45 @@ X = [[188, 80, 44],
      [175, 64, 39],
      [177, 70, 40]]
 
-Y = [genders[0], genders[0], genders[0], genders[1], genders[1], genders[1],
-     genders[1], genders[0], genders[1], genders[1]]
+Y = [genders[0],
+     genders[0],
+     genders[0],
+     genders[1],
+     genders[1],
+     genders[1],
+     genders[1],
+     genders[0],
+     genders[1],
+     genders[1]]
 
+
+scaler = preprocessing.StandardScaler().fit(X)
 X_scalled = preprocessing.scale(X)
 
-print(X_scalled)
+print(scaler.mean_)
 ## CHALLENGE - create 3 more classifiers...
 #1
 clf = tree.DecisionTreeClassifier()
 #2
 gnb = GaussianNB()
+#3
+svmClf = svm.SVC()
 
 ## CHALLENGE - train all of it
 #1
-clf = clf.fit(X, Y)
+clf = clf.fit(X_scalled, Y)
 #2
-gnb = gnb.fit(X, Y)
-
+gnb = gnb.fit(X_scalled, Y)
+#3
+svmClf.fit(X_scalled, Y)
 #data for test
-inputs = [[190, 70, 43], [201, 97, 46], [167, 51, 39]]
+inputs = [[190, 84, 43], [201, 97, 46], [167, 51, 39]]
 
 prediction = clf.predict(inputs)
 print(prediction)
 
 prediction = gnb.predict(inputs)
+print(prediction)
+
+prediction = svmClf.predict(inputs)
 print(prediction)
